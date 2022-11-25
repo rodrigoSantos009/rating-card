464d4739b803a9ea98a3d62400c0c6d8e83c06e1
Rodrigo <009rodrigo009@gmail.com>
2022-11-25 13:14:57 -0300

    Adding flip card

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 let ratings = document.querySelectorAll(".rating");
 let btn = document.querySelector("#button");
+const cards = document.querySelector("#cards")
 let thankYouCard = document.querySelector("#secundary-card");
 let ratedCard = document.querySelector("#main-card");
 let span = document.querySelector("#rated-value ");
+
+const flipCard = () => {
+  cards.classList.toggle("flipCard")
+}
 
 for (let i = 0; i < ratings.length; i++) {
   ratings[i].addEventListener("click", () => {
@@ -15,9 +20,12 @@ for (let i = 0; i < ratings.length; i++) {
     ratings[i].classList.add("selected");
 
     btn.addEventListener("click", () => {
-      ratedCard.style.display = "none";
-      thankYouCard.style.display = "flex";
-      span.innerHTML = rating;
+      flipCard();
+      setTimeout(() => {
+        ratedCard.style.display = "none";
+        thankYouCard.style.display = "flex";
+        span.innerHTML = rating;
+      }, 170)
     });
   });
 }
